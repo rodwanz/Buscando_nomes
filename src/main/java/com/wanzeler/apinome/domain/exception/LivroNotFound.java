0a5e6e649041a3da4ec60eafcae1adfd48fde60b
Rodrigo Wanzeler <wanzervai@gmail.com>
2023-07-14 20:32:06 -0300

    Criando o banco de dados e suas tabelas e fazendo a migração com Flyway

--- src/main/java/com/wanzeler/apinome/domain/exception/LivroNotFound.java
+++ src/main/java/com/wanzeler/apinome/domain/exception/LivroNotFound.java
@@ -1,9 +1,5 @@
 package com.wanzeler.apinome.domain.exception;
 
-public class LivroNotFound extends RuntimeException{
-	private static final long serialVersionUID = 1L;
-	
-	public LivroNotFound(String message) {
-		super();
-	}
+public class LivroNotFound{
+
 }
